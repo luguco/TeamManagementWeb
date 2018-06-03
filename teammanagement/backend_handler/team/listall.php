427211114dfefae59f1a0483e47c6b9caa5c6d10
mystudios <syant@web.de>
2018-06-03 13:15:41 +0200

    Css editing on teamsite

--- teammanagement/backend_handler/team/listall.php
+++ teammanagement/backend_handler/team/listall.php
@@ -13,15 +13,25 @@ $stmt->execute();
 
 $erg = $stmt->fetchAll();
 foreach ($erg as $row) {
-    echo "        <dt><a href='team.php?group=" . $row['Name'] . "'>" . $row['Name'] . "</a></dt>\n";
+    $stmt = $gdbh->prepare("SELECT Colors.Colorhash FROM Colors, Groups WHERE Colors.Colorname = Groups.Rankcolor AND Groups.Name = :Group");
+    $stmt->bindParam(":Group", $row['Name']);
+    $stmt->execute();
+    $res = $stmt->fetch();
+
+    echo "        <dt style='background-color: " . $res['Colorhash'] . "'><a href='team.php?group=" . $row['Name'] . "'>" . $row['Name'] . "</a></dt>\n";
 
     $stmt = $gdbh->prepare("SELECT User.Username FROM Groups, User, GroupPlayer WHERE GroupPlayer.G_ID = Groups.G_ID AND GroupPlayer.P_ID = User.UUID AND Groups.Name = :Group");
     $stmt->bindParam(":Group", $row['Name']);
     $stmt->execute();
     $eg = $stmt->fetchAll();
 
+    $stmt = $gdbh->prepare("SELECT Colors.Colorhash FROM Colors, Groups WHERE Colors.Colorname = Groups.Usercolor AND Groups.Name = :Group");
+    $stmt->bindParam(":Group", $row['Name']);
+    $stmt->execute();
+    $res = $stmt->fetch();
+
     foreach ($eg as $rw) {
-        echo "            <dd><a href='team.php?user=" . $rw['Username'] . "'>" . $rw['Username'] . "</a></dd>\n";
+        echo "            <dd style='background-color: " . $res['Colorhash'] . "'><a href='team.php?user=" . $rw['Username'] . "'>" . $rw['Username'] . "</a></dd>\n";
     }
 }
 echo "    </dl>\n";
